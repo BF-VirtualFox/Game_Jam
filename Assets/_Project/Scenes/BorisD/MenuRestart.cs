065665807e69a3c6637d92a5441513ace0127833
bdeTheux <boris.detheux@student.vinci.be>
2021-11-02 21:48:51 +0100

    GUI clickable && redirection
    - Player fall when restart

---
+++ Assets/_Project/Scenes/BorisD/MenuRestart.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MenuRestart : MonoBehaviour
+{    
+    [SerializeField] private UnityEvent OnDeath;
+    [SerializeField] private int currentHealth;
+
+    void Menu()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("hi");
+            currentHealth -= 5;
+            if (currentHealth == 0)
+            {
+                OnDeath?.Invoke();
+                Destroy(gameObject);
+            }
+        }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Menu();
+    }
+}
